bba16423707bcad9a9829117f8ba89ca626e3979
Stepan Greenberg <stepan.greenberg@ya.ru>
2022-07-19 00:13:47 +0300

    refactored

--- foods/views.py
+++ foods/views.py
@@ -7,25 +7,21 @@ from .models import Food, FoodCategory
 
 class FoodCategoryListAPIView(generics.ListAPIView):
     serializer_class = FoodListSerializer
+    queryset = (
+        FoodCategory.objects
 
-    def get_queryset(self):
-        food_category_queryset = (
-            FoodCategory.objects
+        # Фильтруем категории (если нет опубликованной еды - не выводим категорию)
+        .filter(food__is_publish=True)
+        .annotate(food_published_count=Count("food"))
+        .filter(food_published_count__gte=0)
 
-            # Фильтруем категории (если нет опубликованной еды - не выводим категорию)
-            .filter(food__is_publish=True)
-            .annotate(food_published_count=Count("food"))
-            .filter(food_published_count__gte=0)
-
-            # Фильтруем опубликованную еду (если еда не опубликована - не выводим)
-            # Результат заносим в food_filtered для вывода через FoodListSerializer
-            .prefetch_related(
-                Prefetch(
-                    "food",
-                    queryset=Food.objects.filter(is_publish=True),
-                    to_attr="food_filtered",
-                )
+        # Фильтруем опубликованную еду (если еда не опубликована - не выводим)
+        # Результат заносим в food_filtered для вывода через FoodListSerializer
+        .prefetch_related(
+            Prefetch(
+                "food",
+                queryset=Food.objects.filter(is_publish=True),
+                to_attr="food_filtered",
             )
         )
-
-        return food_category_queryset
+    )
